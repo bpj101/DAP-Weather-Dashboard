ae39ee83c7ad7a721aec5a87714bb78d9b90a592
Paul Julian <bpj101@dameagnesproductions.com>
2016-01-01 17:31:08 -0800

    Set up search for city and view to layout cities list

--- dap/data/weatherSrv.js
+++ dap/data/weatherSrv.js
@@ -1,13 +1,28 @@
 (function(){
   'use strict';
   angular.module('dap.data')
-    .factory('weatherSrv', ['', function(){
+    .factory('weatherSrv', ['$http', '$q', function($http, $q){
       return {
         find: findByLocation
       };
 
-      function findByLocation(arguments) {
-        url = 
+      function findByLocation(location) {
+        var appid = '&appid=5f2ff633ab799b31e91a932d1c0c7dde';
+        var url = 'http://api.openweathermap.org/data/2.5/find?q=' + location + appid;
+        var deferred = $q.defer();
+
+        $http.get(url)
+          .success((response) => {
+            // Resolve Promise
+            deferred.resolve(response);
+          })
+          .error((err) => {
+            // Reject Promis
+            deferred.reject(err);
+          });
+
+        return deferred.promise;
       }
+
     }]);
 }());
--- dap/search/searchCtrl.js
+++ dap/search/searchCtrl.js
@@ -2,11 +2,21 @@
   'use strict';
 
   angular.module('dap.search')
-    .controller('Search', ['$scope', function($scope){
+    .controller('Search', ['$scope','weatherSrv', function($scope, weatherSrv){
+      $scope.cities = null;
       $scope.$on('search', search);
 
       function search(evt, data) {
         console.log(data.str);
+        weatherSrv.find(data.str)
+          .then((response) => {
+            // Success
+            console.log(response);
+            $scope.cities = response.list;
+          }, (err) => {
+            // Error
+            console.log('error finding cities: ', err);
+          });
       }
     }]);
 
